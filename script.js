e6b68037a1f5d8a825a6777f8d3ede9a26becb7e
Ashish <ashishmasih92g@gmail.com>
2025-05-22 11:16:38 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,1 +1,44 @@
-//your JS code here. If required.
+
+
+function chainPromise(){
+let p = new Promise((resolve,reject)=>{
+	setTimeout(()=>{
+		resolve([1,2,3,4])
+	},3000)
+})
+		p.then(res =>{
+	return new Promise((resolve,reject)=>{
+		setTimeout(()=>{
+			let even = res.filter((v)=>{
+				return v%2===0
+			})
+				let div = document.createElement("div")
+			div.id = "output"
+		div.textContent = even.join("")
+	document.body.append(div)
+			resolve(even)
+		},1000)
+	})
+})
+.then(data=>{
+	return new Promise((resolve,reject)=>{
+	setTimeout(()=>{
+let multiple = data.map((v)=>{
+	return v*2
+})
+
+		let div = document.querySelector("#output")
+		div.textContent = multiple.join("")
+		resolve(multiple)
+		
+	},3000)
+		
+	})
+})
+	.catch(err=>{
+		console.log(err.message)
+	})
+
+}
+
+chainPromise()
